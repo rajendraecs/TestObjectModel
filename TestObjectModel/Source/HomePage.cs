1cb777e46ce70fd9ff5e42f256aa23ba6fd9f92f
503218292-Rajendra Bishoyi <rajendra.bishoyi@ge.com>
2024-01-07 20:32:06 +0530

    Add project files.

---
+++ TestObjectModel/Source/HomePage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Id, Using = "twotabsearchtextbox")]
+        private IWebElement _searchtxtbox;
+
+        [FindsBy(How = How.Id, Using = "nav-search-submit-button")]
+        private IWebElement _searchbtn;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Search(string searchText)
+        {
+            _searchtxtbox.SendKeys(searchText);
+            _searchbtn.Click();
+        }
+
+    }
+}
---
+++ TestObjectModel/Test/HomeTest.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjectModel.Source.Pages;
+
+
+namespace PageObjectModel.Tests
+{
+    public class HomeTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitScript()
+        {
+            _driver = new ChromeDriver();
+        }
+        
+        [Test]
+        public void SearchBrowserStack()
+        {
+            HomePage hp = new HomePage(_driver);
+            _driver.Navigate().GoToUrl("https://www.amazon.in/");
+            _driver.Manage().Window.Maximize();
+            hp.Search("selenium book");
+            Assert.True(_driver.Title.Contains("selenium book"));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
